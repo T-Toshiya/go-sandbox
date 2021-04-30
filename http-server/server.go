74e5b20849bffa06ea0fff36ba2852563b69b025
toshiya <toshiya.tanaka0522@gmail.com>
2021-04-30 15:18:09 +0900

    add another score

---
+++ http-server/server.go
@@ -0,0 +1,19 @@
+package http_server
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+	}
+}
