c6f4cfee9e04dbccad6457e0a937c3d3a8cb09f4
T-Toshiya <toshiya.tanaka@connehito.com>
2022-06-09 22:27:53 +0900

    引数パターンとレシーバーパターンを実装

---
+++ practical-go/receiver.go
@@ -0,0 +1,21 @@
+package main
+
+type SensorData struct {
+	SensorType string
+	ModelID    string
+	Value      float32
+}
+
+func ReadValue1(r SensorData) float32 {
+	if r.SensorType == "Fahrenheit" {
+		return (r.Value * 9 / 5) + 32
+	}
+	return r.Value
+}
+
+func (r SensorData) ReadValue2() float32 {
+	if r.SensorType == "Fahrenheit" {
+		return (r.Value * 9 / 5) + 32
+	}
+	return r.Value
+}
