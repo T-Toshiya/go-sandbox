f304c8c20b5c826fc27b8144979bdc3be8141ae0
T-Toshiya <toshiya.tanaka@connehito.com>
2022-06-15 10:10:04 +0900

    構造体をポインタ型として扱う

---
+++ practical-go/struct-design.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type NoCopyStruct struct {
+	self  *NoCopyStruct
+	Value *string
+}
+
+func NewNoCopyStruct(value string) *NoCopyStruct {
+	r := &NoCopyStruct{
+		Value: &value,
+	}
+	r.self = r
+	return r
+}
+
+func (n *NoCopyStruct) String() string {
+	if n != n.self {
+		panic("should not copy NoCopyStruct instance without Copy() method")
+	}
+	return *n.Value
+}
+
+func main() {
+	ncs := NewNoCopyStruct("NoCopy")
+
+	fmt.Println(ncs)
+}
