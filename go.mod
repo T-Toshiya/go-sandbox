f7e5a91b21783c79794b70b4445f41bac2162cbb
T-Toshiya <toshiya.tanaka@connehito.com>
2022-06-18 15:00:09 +0900

    インターフェースを実装

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/T-Toshiya/go-sandbox
 
 go 1.14
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require (
+	github.com/gen2brain/beeep v0.0.0-20220518085355-d7852edf42fc // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
---
+++ practical-go/interface.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/gen2brain/beeep"
+)
+
+type Warning interface {
+	Show(message string)
+}
+
+type ConsoleWarning struct {
+}
+
+func (c ConsoleWarning) Show(message string) {
+	fmt.Fprintf(os.Stderr, "[%s]: %\n", os.Args[0], message)
+}
+
+type DesktopWarning struct {
+}
+
+func (d DesktopWarning) Show(message string) {
+	beeep.Alert(os.Args[0], message, "")
+}
+
+type SlackWarning struct {
+	URL     string
+	Channel string
+}
+
+type slackMessage struct {
+	Text      string `json:"text"`
+	Username  string `json:"username"`
+	IconEmoji string `json:"icon_emoji"`
+	Channel   string `json:"channel"`
+}
+
+func (s SlackWarning) Show(message string) {
+	params, _ := json.Marshal(slackMessage{
+		Text:      message,
+		Username:  os.Args[0],
+		IconEmoji: ":robot_face",
+		Channel:   s.Channel,
+	})
+
+	resp, err := http.PostForm(
+		s.URL,
+		url.Values{"payload": {string(params)}},
+	)
+	if err == nil {
+		io.ReadAll(resp.Body)
+		defer resp.Body.Close()
+	}
+}
+
+func main() {
+	var warn Warning
+
+	warn = &ConsoleWarning{}
+	warn.Show("Hello World to console")
+
+	warn = &DesktopWarning{}
+	warn.Show("Hello World to desktop")
+
+	warn = &SlackWarning{
+		URL:     os.Getenv("SLACK_URL"),
+		Channel: "#general",
+	}
+	warn.Show("Hello World to slack")
+}
